b6e7c7db4c81f25e353bb8e67688963375ef6dc1
YMOHANTY <yasowant@live.com>
2021-10-16 19:16:20 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+from turtle import Turtle, Screen
+from random import randint, choice
+import heroes
+
+tim = Turtle()
+
+tim.shape("turtle")
+
+screen = Screen()
+screen.colormode(255)
+
+direction = [0, 90, 180, 270]
+
+# count = 0
+# while count < 4:
+#     tim.forward(100)
+#     tim.right(90)
+#     count += 1
+#
+
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+
+def draw_shape(num):
+    angle = 360/num
+    for _ in range(num):
+        tim.forward(100)
+        tim.right(angle)
+
+def draw_random_walk():
+    tup = (randint(0, 255), randint(0, 255), randint(0, 255))
+    tim.pencolor(tup)
+    tim.forward(25)
+    tim.setheading(choice(direction))
+
+shape = {
+    "triangle": 3,
+    "square": 4,
+    "pentagon": 5,
+    "hexagon": 6,
+    "heptagon": 7,
+    "octagon": 8,
+    "nonagon": 9,
+    "decagon": 10
+}
+
+# tim.pencolor("Blue")
+# for _ in range(100):
+#     if _ % 2 == 0:
+#         tim.pendown()
+#         tim.forward(5)
+#     else:
+#         tim.penup()
+#         tim.forward(5)
+tim.speed(10)
+tim.pensize(10)
+# for keys in shape:
+#     tup = (randint(0, 255), randint(0, 255), randint(0, 255))
+#     tim.pencolor(tup)
+#     # tim.pencolor("red")
+#     draw_shape(shape[keys])
+count = 0
+while(count < 100):
+    draw_random_walk()
+    count += 1
+
+
+print(tim.speed())
+print(tim.pensize())
+screen.exitonclick()
